469e10a9640b2e32be17d462265afb2380a74d02
Eman <eman.a.ziada@gmail.com>
2025-10-07 02:15:15 +0300

    add navbar component, home page and layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import './index.css'
+// import App from './App.tsx'
+// import { createBrowserRouter } from 'react-router';
+// import { RouterProvider } from 'react-router/dom'
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import LayOut from './layout/layout.tsx';
+import About from "./pages/about/about.tsx";
+import Products from "./pages/products/products.tsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const rout = createBrowserRouter([
+  { path: "/", element: <LayOut/>, children: [
+      { path: "about", element: <About /> },
+      { path: "product", element: <Products /> }
+],
+},
+])
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={rout}></RouterProvider>
   </StrictMode>,
 )
